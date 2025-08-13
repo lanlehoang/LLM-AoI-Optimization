cbb0efefbca3d4490f69719692c0383a2db68788
lanlehoang <lelan210420@gmail.com>
2025-08-13 23:21:45 +0700

    init the env

--- src/env.py
+++ src/env.py
@@ -3,6 +3,19 @@ from gym.spaces import Box, Discrete
 import numpy as np
 from src.utils.generators import *
 from src.utils.get_config import get_system_config
+import heapq    # Used for event priority queue
+from enum import Enum
+
+
+class EventType(Enum):
+    """
+    Enum for different types of events in the satellite environment.
+    Types of events:
+    - PROCESS: A package is processed by satellite i at time t
+    - TRANSFER: A package is transferred from satellite i to j at time t
+    """
+    PROCESS = "process"
+    TRANSFER = "transfer"    
 
 
 class SatelliteEnv(Env):
@@ -13,8 +26,36 @@ class SatelliteEnv(Env):
         super().__init__()
         self.system_config = get_system_config()
 
+        # Initialize satellites
+        np.random.seed(RANDOM_SEED) # Set random seed each time the environment is created
+        self.satellite_pos = generate_satallite_positions(self.system_config['n_satellites'])
+        self.satellite_mu = generate_satellite_processing_rates(self.system_config['n_satellites'])
+        start, end = choose_start_end(self.satellite_pos)
+        self.start_satellite = start
+        self.end_satellite = end
+
+        # Time and event management
+        self.time = 0
+        self.event_queue = []
+
+        # Action space
+        self.action_space = None  # To be decided
+
     def reset(self):
-        pass
+        """
+        Reset the environment to its initial state.
+        """
+        # Initialize satellites
+        np.random.seed(RANDOM_SEED) # Set random seed each time the environment is created
+        self.satellite_pos = generate_satallite_positions(self.system_config['n_satellites'])
+        self.satellite_mu = generate_satellite_processing_rates(self.system_config['n_satellites'])
+        start, end = choose_start_end(self.satellite_pos)
+        self.start_satellite = start
+        self.end_satellite = end
+
+        # Time and event
+        self.time = 0
+        self.event_queue = []
 
     def step(self, action):
         pass
--- src/utils/generators.py
+++ src/utils/generators.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+RANDOM_SEED = 42
+
 
 def generate_satallite_positions(num_satellites, radius):
     """
@@ -28,3 +30,20 @@ def generate_processing_time(mu):
     Generate processing times for a given mean (mu) using an exponential distribution.
     """
     return np.random.exponential(1/mu)
+
+
+def choose_start_end(satellite_positions):
+    """
+    Choose start and end satellites for a transfer operation.
+    Ensure that the angle between the two satellites is greater than 135 degrees.
+    Return (start_index, end_index).
+    """
+    n_satellites = satellite_positions.shape[0]
+    for i in range(n_satellites):
+        for j in range(i + 1, satellite_positions.shape[0]):
+            angle = np.arccos(
+                np.dot(satellite_positions[i], satellite_positions[j]) / \
+                (np.linalg.norm(satellite_positions[i]) * np.linalg.norm(satellite_positions[j]))
+            )
+            if angle > 3*np.pi/4:  # 135 degrees in radians
+                return i, j
