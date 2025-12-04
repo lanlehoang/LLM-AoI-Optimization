e8355cb6f3eb4ec199137eca05925941f2d3d391
lanlehoang <lelan210420@gmail.com>
2025-12-04 20:33:48 +0700

    change visualization

--- src/utils/report.py
+++ src/utils/report.py
@@ -18,14 +18,26 @@ def calc_running_mean_adaptive(nums, window_size=4):
     return running_mean
 
 
-def plot_single_line(data_array, plot_title, x_label, y_label, y_ticks, save_path, grid_step=5, is_log_scale=False):
-    plt.figure(figsize=(10, 5))
+def plot_single_line(
+    data_array: np.ndarray | List[float],
+    height: int,
+    width: int,
+    plot_title: str,
+    x_label: str,
+    y_label: str,
+    y_ticks: np.ndarray | List[float],
+    save_path: str,
+    grid_step: int = 5,
+    is_log_scale: bool = False,
+    markersize: int = 4,
+):
+    plt.figure(figsize=(width, height))
     plt.plot(data_array, marker="o", linestyle="-", color="b", label=plot_title)
-    plt.grid(True, which="both", linestyle="--", linewidth=0.5)
+    plt.grid(True, which="both", linestyle="--", linewidth=0.5, markersize=markersize)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.title(plot_title)
-    plt.xticks(np.arange(0, len(data_array), step=grid_step))
+    plt.xticks(np.arange(0, len(data_array) + 1, step=grid_step))
     plt.yticks(y_ticks)
     # Set y-axis to logarithmic scale if specified
     if is_log_scale:
@@ -37,7 +49,9 @@ def plot_single_line(data_array, plot_title, x_label, y_label, y_ticks, save_pat
 
 
 def plot_multiple_lines(
-    data_arrays: List[np.ndarray],
+    data_arrays: List[np.ndarray] | List[List[float]],
+    height: int,
+    width: int,
     plot_title: str,
     x_label: str,
     y_label: str,
@@ -46,10 +60,11 @@ def plot_multiple_lines(
     save_path: str,
     grid_step: int = 1,
     is_log_scale: bool = False,
+    markersize: int = 4,
 ):
-    plt.figure(figsize=(10, 5))
+    plt.figure(figsize=(width, height))
     for i, data_array in enumerate(data_arrays):
-        plt.plot(data_array, marker="o", linestyle="-", label=labels[i])
+        plt.plot(data_array, marker="o", linestyle="-", label=labels[i], markersize=markersize)
     plt.grid(True, which="both", linestyle="--", linewidth=0.5)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
